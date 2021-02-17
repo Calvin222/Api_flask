36c50fb90f6f4d3516a446b70a42e1c3062b27cd
Calvin <37435679+Calvin222@users.noreply.github.com>
2021-02-17 11:58:03 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,14 +1,16 @@
 from flask import Flask
-import socket
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/name')
 def name():
-    name = socket.gethostname()
-    return name
+	out = subprocess.check_output('hostname', shell=True)
+	return out
 
-@app.route('/<x>/<y>')
-def chaine(x,y):
-    return x * y 
+@app.route('/<char>/<nb>')
+def char(char, nb):
+	return char*int(nb)
 
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8080)
